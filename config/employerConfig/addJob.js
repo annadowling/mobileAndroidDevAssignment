34e1d127ba249e47318564b78689c16580a6a6b0
annadowling <anna.dowling@errigal.com>
2017-04-12 16:53:51 +0100

    Added update to addJob

--- config/employerConfig/addJob.js
+++ config/employerConfig/addJob.js
@@ -8,17 +8,17 @@ var employer = require('../../models/employer.js');
 
 exports.addJob = function(employerEmail, employerPhone,jobTitle,jobDescription,address, callback) {
 
-    var newJob = new job({
-        jobTitle: jobTitle,
-        contactNumber: employerPhone,
-        jobDescription: jobDescription,
-        address: address
+    employer.findOne( {email: employerEmail }, function(err, employerEntry){
 
-    });
+        var newJob = new job({
+            jobTitle: jobTitle,
+            contactNumber: employerPhone,
+            jobDescription: jobDescription,
+            address: address
 
-    newJob.save();
-
-    employer.findOneAndUpdate({ email: employerEmail }, { $set: { jobs: newJob } }, function(err, doc) {
-
-    });
+        });
+        newJob.save();
+        employerEntry.jobs.push(newJob);
+        employerEntry.save();
+});
 }
