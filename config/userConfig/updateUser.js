33463e0ee5e5e0e9d212f7ee42c7187570eabdcb
annadowling <anna.dowling@errigal.com>
2017-04-19 13:57:59 +0100

    Added update user routing

--- config/userConfig/updateUser.js
+++ config/userConfig/updateUser.js
@@ -11,18 +11,43 @@ exports.updateUser = function (token, firstName, lastName, password, age, profes
     user.findOne({token: token}, function (err, doc) {
         console.log("user token is " + token);
         if (doc) {
-            callback({
-                'response': "User Found",
-                'email': doc.email,
-                'token': doc.token,
-                'firstName': doc.firstName,
-                'lastName': doc.lastName,
-                'age': doc.age || "",
-                'profession': doc.profession || "",
-                'bio': doc.bio || ""
+            if (firstName && firstName != "") {
+                doc.firstName = firstName;
+            } else if (lastName && lastName != "") {
+                doc.lastName = lastName;
+            } else if (password && password != "") {
+                var temp = rand(160, 36);
+                var newpass = temp + password;
+                var hashed_password = crypto.createHash('sha512').update(newpass).digest("hex");
+                doc.hashed_password = hashed_password;
+            } else if (age && age != "") {
+                doc.age = age;
+            } else if (profession && profession != "") {
+                doc.profession = profession;
+            } else if (bio && bio != "") {
+                doc.bio = bio;
+            }
+
+            doc.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    callback({'response': "Error during save, please try again!"});
+                } else {
+                    callback({
+                        'response': "Updated User",
+                        'email': doc.email,
+                        'token': doc.token,
+                        'firstName': doc.firstName,
+                        'lastName': doc.lastName,
+                        'age': doc.age || "",
+                        'profession': doc.profession || "",
+                        'bio': doc.bio || ""
+                    });
+                }
+
             });
         } else {
-            callback({'response': "Error retrieving user data!"});
+            callback({'response': "Error updating user data!"});
         }
 
     });
